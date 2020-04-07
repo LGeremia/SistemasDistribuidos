29d3e4db55b28577c8f538cc6c533ce91ca702ff
Leonardo Geremia <leonardogeremisc@gmail.com>
2020-04-06 23:30:07 -0300

    adicionado dependentes e iniciado conclusão do projeto

--- SistemaChord/src/exemplos/Listas.java
+++ SistemaChord/src/exemplos/Listas.java
@@ -1,3 +1,5 @@
+package exemplos;
+
 
 import java.util.ArrayList;
 import java.util.List;
--- SistemaChord/src/projeto/Anel.java
+++ SistemaChord/src/projeto/Anel.java
@@ -1,6 +1,7 @@
 package projeto;
 
 import java.nio.file.Files;
+import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,6 +16,8 @@ public class Anel {
 	public static void main(String[] args) throws IOException {
     	No no;
 		List<No> nos = new ArrayList<No>();
+
+		Scanner s = new Scanner(System.in);
 		
 		Path arquivo = Paths.get("/home/geremiapc/workspace/SistemaChord/src/musicas");
 
@@ -52,9 +55,6 @@ public class Anel {
         h = r.size();
         aux = 0;
         //gerar os nós do anel
-        
-        Map<Integer, String> teste = new HashMap<>();
-        teste.put(15, "joao");
 		for(String m: musicas){
 			no = new No();
 			no.setChave(i);
@@ -73,7 +73,7 @@ public class Anel {
 			i++;
         }
 		
-		Map<Integer, String> dependentes= new HashMap<>();
+		Map<Integer, String> dependentes = new HashMap<>();
 		h = 0;
 		aux = 0;
 		i = r.get(h)-1;
@@ -82,17 +82,19 @@ public class Anel {
 				i = nos.size()-1;
 			}else if(i == r.get(0)) {
 				nos.get(r.get(h)).setDependentes(dependentes);
-				dependentes.clear();
+				dependentes = new HashMap<>();
+				//System.out.println(nos.get(r.get(h)).getDependentes());
 				break;
 			}
-			System.out.println(nos.get(i).getChave()+"--"+nos.get(i).getDependentes());
+			//System.out.println(nos.get(h).getChave()+"--"+nos.get(h).getDependentes());
 			if(!nos.get(i).getAtivo()) {
 				int key = nos.get(i).getChave();
 				String value = nos.get(i).getValor();
 				dependentes.put(key, value);
 			}else if(nos.get(i).getAtivo()&&(aux==0)){
 				nos.get(r.get(h)).setDependentes(dependentes);
-				dependentes.clear();
+				dependentes = new HashMap<>();
+				//System.out.println(nos.get(r.get(h)).getDependentes());
 				int key = nos.get(i).getChave();
 				String value = nos.get(i).getValor();
 				dependentes.put(key, value);
@@ -100,7 +102,8 @@ public class Anel {
 				aux = 1;
 			}else if(nos.get(i).getAtivo()&&(h!=0)) {
 				nos.get(r.get(h)).setDependentes(dependentes);
-				dependentes.clear();
+				dependentes = new HashMap<>();
+				//System.out.println(nos.get(r.get(h)).getDependentes());
 				int key = nos.get(i).getChave();
 				String value = nos.get(i).getValor();
 				dependentes.put(key, value);
@@ -108,11 +111,196 @@ public class Anel {
 			}
 			i--;
 		}while(true);
-		/*
-		for(No teste : nos) {
-			System.out.println(nos.get(i).getChave()+"--"+nos.get(i).getDependentes());
-			i++;
-		}*/
+		
+		int p;
+		String valor;
+		do {
+			System.out.println("O que deseja fazer:");
+			System.out.println("1-Ver os nos ativos  2-Adicionar Ativo  3-Remover Ativo  4-Consultar dependentes  5-Sair");
+			p = s.nextInt();
+			if(p == 1){
+				for(Integer e: r) {
+					System.out.println(e);
+				}
+			}else if(p == 2) {
+				System.out.println("Digite a chave que deseja tornar ativa");
+				p = s.nextInt();
+				if(r.contains(p)) {
+					System.out.println("Esse no ja e ativo");
+				}else if (p >= 0 && p < 40) {
+					r.add(p);
+					aux = 0;
+					h = 0;
+			        for(h = 0; h<r.size(); h++){
+			            for(int j = 0; j<(r.size()-1); j++){
+			                if(r.get(j) > r.get(j + 1)){
+			                    aux = r.get(j);
+			                    r.set(j, r.get(j+1));
+			                    r.set(j+1, aux);
+			                }
+			            }
+			        }
+
+			        i = 0;
+			        h = r.size();
+			        aux = 0;
+			        //gerar os nós do anel
+					for(String m: musicas){
+						no = new No();
+						no.setChave(i);
+						no.setValor(m);
+						if(r.contains(i)&&(aux==(h-1))) {
+							no.setAtivo(true);
+							no.setProx(r.get(0));
+							no.setDependentes(null);
+						}else if(r.contains(i)&&(aux<h)) {
+							no.setAtivo(true);
+							no.setProx(r.get(aux+1));
+							no.setDependentes(null);
+							aux++;
+						}
+						nos.add(no);
+						i++;
+			        }
+					
+					dependentes = new HashMap<>();
+					h = 0;
+					aux = 0;
+					i = r.get(h)-1;
+					do {
+						if(i==-1) {
+							i = nos.size()-1;
+						}else if(i == r.get(0)) {
+							nos.get(r.get(h)).setDependentes(dependentes);
+							dependentes = new HashMap<>();
+							//System.out.println(nos.get(r.get(h)).getDependentes());
+							break;
+						}
+						//System.out.println(nos.get(h).getChave()+"--"+nos.get(h).getDependentes());
+						if(!nos.get(i).getAtivo()) {
+							int key = nos.get(i).getChave();
+							String value = nos.get(i).getValor();
+							dependentes.put(key, value);
+						}else if(nos.get(i).getAtivo()&&(aux==0)){
+							nos.get(r.get(h)).setDependentes(dependentes);
+							dependentes = new HashMap<>();
+							//System.out.println(nos.get(r.get(h)).getDependentes());
+							int key = nos.get(i).getChave();
+							String value = nos.get(i).getValor();
+							dependentes.put(key, value);
+							h = r.size() - 1;
+							aux = 1;
+						}else if(nos.get(i).getAtivo()&&(h!=0)) {
+							nos.get(r.get(h)).setDependentes(dependentes);
+							dependentes = new HashMap<>();
+							//System.out.println(nos.get(r.get(h)).getDependentes());
+							int key = nos.get(i).getChave();
+							String value = nos.get(i).getValor();
+							dependentes.put(key, value);
+							h--;
+						}
+						i--;
+					}while(true);
+				}else {
+					System.out.println("Chave invalida");
+				}
+			}else if(p == 3) {
+				System.out.println("Digite a chave do no que deseja remover");
+				p = s.nextInt();
+				if(r.contains(p)){
+					r.remove(p);
+					nos.get(p).setDependentes(null);
+					aux = 0;
+					h = 0;
+			        for(h = 0; h<r.size(); h++){
+			            for(int j = 0; j<(r.size()-1); j++){
+			                if(r.get(j) > r.get(j + 1)){
+			                    aux = r.get(j);
+			                    r.set(j, r.get(j+1));
+			                    r.set(j+1, aux);
+			                }
+			            }
+			        }
+
+			        i = 0;
+			        h = r.size();
+			        aux = 0;
+			        //gerar os nós do anel
+					for(String m: musicas){
+						no = new No();
+						no.setChave(i);
+						no.setValor(m);
+						if(r.contains(i)&&(aux==(h-1))) {
+							no.setAtivo(true);
+							no.setDependentes(null);
+							no.setProx(r.get(0));
+						}else if(r.contains(i)&&(aux<h)) {
+							no.setAtivo(true);
+							no.setProx(r.get(aux+1));
+							no.setDependentes(null);
+							aux++;
+						}
+						nos.add(no);
+						i++;
+			        }
+					
+					dependentes = new HashMap<>();
+					h = 0;
+					aux = 0;
+					i = r.get(h)-1;
+					do {
+						if(i==-1) {
+							i = nos.size()-1;
+						}else if(i == r.get(0)) {
+							nos.get(r.get(h)).setDependentes(dependentes);
+							dependentes = new HashMap<>();
+							//System.out.println(nos.get(r.get(h)).getDependentes());
+							break;
+						}
+						//System.out.println(nos.get(h).getChave()+"--"+nos.get(h).getDependentes());
+						if(!nos.get(i).getAtivo()) {
+							int key = nos.get(i).getChave();
+							String value = nos.get(i).getValor();
+							dependentes.put(key, value);
+						}else if(nos.get(i).getAtivo()&&(aux==0)){
+							nos.get(r.get(h)).setDependentes(dependentes);
+							dependentes = new HashMap<>();
+							//System.out.println(nos.get(r.get(h)).getDependentes());
+							int key = nos.get(i).getChave();
+							String value = nos.get(i).getValor();
+							dependentes.put(key, value);
+							h = r.size() - 1;
+							aux = 1;
+						}else if(nos.get(i).getAtivo()&&(h!=0)) {
+							nos.get(r.get(h)).setDependentes(dependentes);
+							dependentes = new HashMap<>();
+							//System.out.println(nos.get(r.get(h)).getDependentes());
+							int key = nos.get(i).getChave();
+							String value = nos.get(i).getValor();
+							dependentes.put(key, value);
+							h--;
+						}
+						i--;
+					}while(true);
+				}else {
+					System.out.println("Chave invalida!");
+				}
+			}else if(p == 4) {
+				System.out.println("Digite a chave do no principal que deseja consultar");
+				p = s.nextInt();
+				if(nos.get(p).getAtivo()) {
+					dependentes = new HashMap<>();
+					dependentes = nos.get(p).getDependentes();
+					System.out.println(dependentes);
+				}else if(!nos.get(p).getAtivo()) {
+					System.out.println("No invalido como ativo");
+				}
+			}else if(p == 5) {
+				break;
+			}else {
+				System.out.println("Opçao invalida");
+			}
+		}while(true);
 		
 	}
 
